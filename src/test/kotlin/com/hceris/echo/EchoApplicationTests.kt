6c870546151770c95dcfd6b891805304092d62dd
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-02-04 22:03:29 +0100

    initial commit

---
+++ src/test/kotlin/com/hceris/echo/EchoApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.hceris.echo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EchoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
