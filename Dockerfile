7057c19dba1dbfda1cdc14d8b71376c8d201928c
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-02-09 15:33:14 +0100

    add jar to container

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM openjdk:8-jre-alpine3.9
+
+WORKDIR /app
+
+COPY build/libs/echo.jar .
--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -10,4 +10,8 @@ describe 'Container' do
       its(:stderr) { is_expected.to match(/1.8.0_191/) }
     end
   end
+
+  describe file('echo.jar') do
+    it { is_expected.to be_file }
+  end
 end
