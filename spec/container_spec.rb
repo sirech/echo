d386d764fa5c0bddf20066a371aa5edcc6df51fc
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-02-09 15:30:27 +0100

    test java version

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -4,4 +4,10 @@ describe 'Container' do
   describe file('/etc/alpine-release') do
     its(:content) { is_expected.to match(/3.9.0/) }
   end
+
+  describe 'java' do
+    describe command('java -version') do
+      its(:stderr) { is_expected.to match(/1.8.0_191/) }
+    end
+  end
 end
