7ea9700fef4f31d78ad8da286dc110e0bc7acadb
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-03-11 22:36:59 +0100

    add numbers list route

---
+++ src/main/kotlin/com/hceris/echo/ListController.kt
@@ -0,0 +1,29 @@
+package com.hceris.echo
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private const val PATH = "/list"
+
+@RestController
+@RequestMapping(PATH)
+class ListController {
+    @GetMapping("numbers")
+    fun numbersDefault(): ResponseEntity<Numbers> {
+        return ResponseEntity.ok(formatNumbers(5))
+    }
+
+    @GetMapping("numbers/{elements}")
+    fun numbers(@PathVariable("elements") elements: Int): ResponseEntity<Numbers> {
+        val elements = Math.min(elements, 100)
+        return ResponseEntity.ok(formatNumbers(elements))
+    }
+
+    fun formatNumbers(elements: Int) = Numbers((1..elements).toList())
+}
+
+data class Numbers(val elements: List<Number>)
+
---
+++ src/test/kotlin/com/hceris/echo/ListControllerTest.kt
@@ -0,0 +1,39 @@
+package com.hceris.echo
+
+import org.junit.Ignore
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class ListControllerTest(@Autowired val webApplicationContext: WebApplicationContext) {
+    val mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+
+    @Test
+    fun `numbers returns a list of numbers`() {
+        mockMvc.perform(get("/list/numbers")
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(content().json("""{ "elements": [1, 2, 3, 4, 5] }"""))
+                .andExpect(status().isOk)
+    }
+
+    @Test
+    fun `numbers can customize the length of the list`() {
+        mockMvc.perform(get("/list/numbers/7")
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(content().json("""{ "elements": [1, 2, 3, 4, 5, 6, 7] }"""))
+                .andExpect(status().isOk)
+    }
+}
+
