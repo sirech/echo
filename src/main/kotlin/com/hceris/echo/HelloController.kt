a680f5d5d5e1ee92e21e28bee8f90bc53d4b8f98
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-04-09 23:56:17 +0200

    add swagger docs

--- src/main/kotlin/com/hceris/echo/HelloController.kt
+++ src/main/kotlin/com/hceris/echo/HelloController.kt
@@ -1,5 +1,9 @@
 package com.hceris.echo
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 private const val PATH = "/hello"
 
 @RestController
-@RequestMapping(PATH)
+@RequestMapping(PATH, produces = [MediaType.APPLICATION_JSON_VALUE])
+@Api(value = "Hello There!", description = "This is a polite controller")
 class HelloController {
     @GetMapping("")
     fun world(): ResponseEntity<Salutation> {
@@ -17,7 +22,8 @@ class HelloController {
     }
 
     @GetMapping("{name}")
-    fun there(@PathVariable("name") name: String): ResponseEntity<Salutation> {
+    @ApiOperation(value = "Greets you back")
+    fun there(@ApiParam("Your name") @PathVariable("name") name: String): ResponseEntity<Salutation> {
         return ResponseEntity.ok(Salutation(name))
     }
 }
