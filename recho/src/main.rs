657e38caed3279e5364844d7d4ef91b311961442
Mario Fernandez <mario.fernandez@hceris.com>
2020-06-18 23:40:01 +0200

    basic use of clap

--- recho/src/main.rs
+++ recho/src/main.rs
@@ -1,17 +1,32 @@
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
-use std::collections::HashMap;
 
-fn request(host: &str) -> Result<HashMap<String, String>, reqwest::Error> {
-    reqwest::blocking::get(&format!("{}/fish/tur", host))?
-        .json()
+#[macro_use]
+extern crate clap;
+
+fn request(host: &str, query: &str) -> Result<HashMap<String, String>, reqwest::Error> {
+    reqwest::blocking::get(&format!("{}/fish/{}", host, query))?.json()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let host = env::var("HOST")?;
-    let body = request(&host)?;
+    let matches = clap_app!(app =>
+                            (version: "0.0.1")
+                            (@subcommand fish =>
+                             (about: "fish in different languages")
+                             (@arg QUERY: +required "Query String")
+                            )
+    )
+    .get_matches();
 
-    println!("{:?}", body);
+    if let Some(matches) = matches.subcommand_matches("fish") {
+        let query = matches.value_of("QUERY").unwrap();
+
+        let host = env::var("HOST")?;
+        let body = request(&host, &query)?;
+
+        println!("{:?}", body);
+    }
 
     Ok(())
 }
